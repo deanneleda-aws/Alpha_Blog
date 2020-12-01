5b461e19bb2fefa836269c2e390c0f783d9bd522
Deanne Eleda <deanne.eleda@awsys-i.com>
2020-12-01 17:01:15 +0800

    Add users creation action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_020520) do
+ActiveRecord::Schema.define(version: 2020_12_01_034737) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_020520) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
